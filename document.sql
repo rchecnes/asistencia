e842f6864a5ae4fac6f19a9bc09ce4f532c82583
rchecnes <richardchec@gmail.com>
2016-12-13 14:26:09 -0500

    add sql

---
+++ document.sql
@@ -0,0 +1,5 @@
+-- Menu
+INSERT INTO menu(padre,nombre,enlace,estado)VALUES(0,'Asistencia','asistencia',1);
+INSERT INTO menu(padre,nombre,enlace,estado)VALUES(0,'Usuarios','user',1);
+INSERT INTO menu(padre,nombre,enlace,estado)VALUES(0,'Reporte','',1);
+INSERT INTO menu(padre,nombre,enlace,estado)VALUES(3,'Asistencia','regasistencia',1);
